ea88d846ffe2661d5cd684f2250b55d56906c249
JosiahSchwahn <schwahn.josiah@gmail.com>
2023-04-02 16:13:35 -0600

    Learning webpack to bundle JS modules

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _loadDash from 'lodash';
+
+// import your function
+import myName from './myName';
+
+function component() {
+  const element = document.createElement('div');
+
+  // use your function!
+  element.textContent = myName('Cody');
+  return element;
+}
+
+document.body.appendChild(component());
