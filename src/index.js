297854152aef28d2e4e53427c81b6aec474026c1
JosiahSchwahn <schwahn.josiah@gmail.com>
2023-04-02 18:09:36 -0600

    webpack guide, start - output management

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import _loadDash from 'lodash';
+import './style.css';
+import Icon from './icon.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
+
 
 // import your function
 import myName from './myName';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // use your function!
-  element.textContent = myName('Cody');
+  element.textContent = myName('Josiah Schwahn');
+  element.classList.add('hello');
+
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn)
+  
+  //logs of csv and data.xml, imported from adding our webpack config and import statement above
+  console.log(Data);
+  console.log(Notes);
+
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,44 @@
-const path = require('path');
+ const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+// chained module loaders 
+ module.exports = {
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
   },
-};
+   output: {
+    filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+ 
+  module: {
+    rules: [
+       // this module loader allows us to import css into 
+      // our javascript modules.
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      //this one allows for images and icons
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      //font loader
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      // loading data (XML and CSV)
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+      
+    ],
+  },
+ };
